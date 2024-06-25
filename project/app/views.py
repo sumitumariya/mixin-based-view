2ab82d8f1df9ab4f96ffba132e726a521e9f06a0
sumitumariya <sumitumariya11@gmail.com>
2024-06-26 00:21:03 +0530

    views.py

--- project/app/views.py
+++ project/app/views.py
@@ -1,50 +1,50 @@
-from .models import Student
-from .serializers import StudentSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# from .models import Student
+# from .serializers import StudentSerializer
+# from django.http import Http404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
 
 
-class Stu_List(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-    def get(self, request):
-        student = Student.objects.all()
-        serializer = StudentSerializer(student, many=True)
-        return Response(serializer.data)
+# class Stu_List(APIView):
+#     """
+#     List all snippets, or create a new snippet.
+#     """
+#     def get(self, request):
+#         student = Student.objects.all()
+#         serializer = StudentSerializer(student, many=True)
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-class Stu_Detail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Student.objects.get(pk=pk)
-        except Student.DoesNotExist:
-            raise Http404
+#     def post(self, request):
+#         serializer = StudentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class Stu_Detail(APIView):
+#     """
+#     Retrieve, update or delete a snippet instance.
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return Student.objects.get(pk=pk)
+#         except Student.DoesNotExist:
+#             raise Http404
 
-    def get(self, request, pk, format=None):
-        Student = self.get_object(pk)
-        serializer = StudentSerializer(Student)
-        return Response(serializer.data)
+#     def get(self, request, pk, format=None):
+#         Student = self.get_object(pk)
+#         serializer = StudentSerializer(Student)
+#         return Response(serializer.data)
 
-    def put(self, request, pk):
-        Student = self.get_object(pk)
-        serializer = StudentSerializer(Student, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         Student = self.get_object(pk)
+#         serializer = StudentSerializer(Student, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        Student = self.get_object(pk)
-        Student.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         Student = self.get_object(pk)
+#         Student.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
